64135e0058aaaef539b5ba5c726497aef3a6fab7
Peter Cervienka <peter.cervienka@gmail.com>
2018-07-09 21:11:32 +0200

    refactor: rename class to className

--- src/about_us/AboutUs.js
+++ src/about_us/AboutUs.js
@@ -2,30 +2,30 @@ import React from 'react';
 
 const AboutUs = () => (
     <div id="fh5co-couple">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
                     <h2>Ahojte!</h2>
                     <h3>6. Októbra 2018, Prievidza, Slovensko</h3>
                     <p>Pozývame vás osláviť s nami našu svadbu</p>
                 </div>
             </div>
-            <div class="couple-wrap animate-box">
-                <div class="couple-half">
-                    <div class="groom">
-                        <img src="images/peti.jpg" alt="groom" class="img-responsive" />
+            <div className="couple-wrap animate-box">
+                <div className="couple-half">
+                    <div className="groom">
+                        <img src="images/peti.jpg" alt="groom" className="img-responsive" />
                     </div>
-                    <div class="desc-groom">
+                    <div className="desc-groom">
                         <h3>Peter Červienka</h3>
                         <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove</p>
                     </div>
                 </div>
-                <p class="heart text-center"><i class="icon-heart2"></i></p>
-                <div class="couple-half">
-                    <div class="bride">
-                        <img src="images/leni.jpg" alt="groom" class="img-responsive" />
+                <p className="heart text-center"><i className="icon-heart2"></i></p>
+                <div className="couple-half">
+                    <div className="bride">
+                        <img src="images/leni.jpg" alt="groom" className="img-responsive" />
                     </div>
-                    <div class="desc-bride">
+                    <div className="desc-bride">
                         <h3>Lenka Hohošová</h3>
                         <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove</p>
                     </div>
